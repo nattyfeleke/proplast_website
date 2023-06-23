3ac09f422ac76d131f5cf758c6bb62047884bd57
Versavvy Engineering <nfeleke568@gmail.com>
2023-06-23 11:20:17 +0300

    completed contact form functionality

--- components/Contactus.vue
+++ components/Contactus.vue
@@ -1,6 +1,21 @@
 <template>
  <div>
-   
+   <div class="notification not-visible " id="notification">
+    <div id='msg-sent' class="msg-sent hidden">
+      <div class="tick">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><g stroke-width="0" id="SVGRepo_bgCarrier"></g><g stroke-linejoin="round" stroke-linecap="round" id="SVGRepo_tracerCarrier"></g><g id="SVGRepo_iconCarrier"> <path stroke-linejoin="round" stroke-linecap="round" stroke-width="1.5" stroke="#fff" d="M20 7L9.00004 18L3.99994 13"></path> </g></svg>
+      </div> 
+    <h4 class="notfication-text">Email Sent</h4> 
+    </div>
+    <div id='msg-notsent' class="msg-notsent hidden">
+      <div class="warn">
+        <svg aria-hidden="true" stroke="white" stroke-width="1.5" viewBox="0 0 24 24" fill="none">
+                <path d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" stroke-linejoin="round" stroke-linecap="round"></path>
+              </svg>
+      </div> 
+    <h4 class="notfication-text">Couldn't send message. Please try again!</h4> 
+    </div>
+   </div>
       <section class="contact">
         <div class="container">
           <!-- <h2>Contact Us</h2> -->
@@ -24,38 +39,46 @@
                       <div class="box">
                         <label for="firstname">First Name</label>
                       </div>
-                      <input type="text" name="firstname" id="firstname" />
+                      <input type="text" name="firstname" id="firstname" required/>
                     </div>
 
                     <div class="box-column">
                       <div class="box">
                         <label for="lastname">Last Name</label>
                       </div>
-                      <input type="text" name="lastname" id="lastname" />
+                      <input type="text" name="lastname" id="lastname" required/>
                     </div>
                     <div class="box-column">
                       <div class="box">
                         <label for="email">Email</label>
                       </div>
-                      <input type="email" name="email" id="email" />
+                      <input type="email" name="email" id="email" required/>
                     </div>
 
                     <div class="box-column">
                       <div class="box">
                         <label for="phone">Phone Number</label>
                       </div>
-                      <input type="tel" name="phone" id="phone" />
+                      <input type="tel" name="phone" id="phone" required/>
                     </div>
+                  </div>
+                  <div class="box-column">
+                    <div class="box">
+                        <label for="subject">Subject</label>
+                      </div>
+                      <input type="text" name="subject" id="subject" required />
                   </div>
                   <div class="box-column">
                     <div class="box">
                       <label for="message">Messages</label>
                     </div>
-                    <textarea name="message" id="message"></textarea>
+                    <textarea name="message" id="message" required></textarea>
                   </div>
 
                   <div class="submit-buttons">
-                    <button type="submit" class="btn btn-secondary">Submit</button>
+                    <button type="submit" class="btn btn-secondary" id="submitmessage">Submit
+                     
+                    </button>
                   </div>
                 </form>
               </div>
@@ -141,26 +164,86 @@ const form = document.querySelector('form');
 console.log('form')
 console.log(form)
 // Add a listener to the submit event of the form
-form.addEventListener('submit', (event) => {
+form.addEventListener('submit', async(event) => {
+  console.log('button clicked')
   // Prevent the default form submission behavior 
   event.preventDefault();
-
+  const notification = document.querySelector('#notification');
+  const sentMsg = document.querySelector('#msg-sent');
+  const notsentMsg = document.querySelector('#msg-notsent');
+  const submitMsgBtn = document.querySelector('#submitmessage');
   const firstname = document.querySelector('#firstname').value;
   const lastname = document.querySelector('#lastname').value;
   const email = document.querySelector('#email').value;
   const phone = document.querySelector('#phone').value;
+  const subject = document.querySelector('#subject').value;
   const message = document.querySelector('#message').value;
 
-  // Create an object with the input values as properties
   const formData = {
-    firstname,lastname,email,phone,message
+    firstname,lastname,email,phone,subject,message
   };
 
-  // Log the form data to the console (for testing purposes)
   console.log(formData);
-//  axios.post()
-  // Submit the form using the submit() method on the form element
-//   form.submit();
+  submitMsgBtn.textContent='Submitting...';
+  submitMsgBtn.disabled=true;
+  submitMsgBtn.classList.add('btn-disabled');
+  try {
+   const res = await axios.post('https://report.proplast.et/email/', formData);
+ console.log(res); 
+ if(res.status=='200'){
+  console.log('success')
+  //clear the form input fields
+  document.querySelector('#firstname').value='';
+  document.querySelector('#lastname').value='';
+  document.querySelector('#email').value='';
+  document.querySelector('#phone').value='';
+  document.querySelector('#subject').value='';
+  document.querySelector('#message').value='';
+  submitMsgBtn.textContent='Submit Message';
+  submitMsgBtn.disabled=false;
+  submitMsgBtn.classList.remove('btn-disabled');
+  notification.classList.remove('not-visible');
+  notification.classList.add('notification-success');
+  sentMsg.classList.remove('hidden');
+  setTimeout(() => {
+    notification.classList.add('not-visible');
+    notification.classList.remove('notification-success');    
+    sentMsg.classList.add('hidden');
+  }, 3000);
+ }else{
+   console.log('Message not sent')
+   submitMsgBtn.textContent='Submit';
+   submitMsgBtn.disabled=false;
+   submitMsgBtn.classList.remove('btn-disabled');
+  notification.classList.remove('not-visible');
+  notification.classList.add('notification-error');
+  notsentMsg.classList.remove('hidden');
+  setTimeout(() => {
+    notification.classList.add('not-visible');
+    notification.classList.remove('notification-error');    
+    notsentMsg.classList.add('hidden');
+  }, 3000);
+
+ }
+  } catch (error) {
+    console.log('Message not sent')
+   submitMsgBtn.textContent='Submit';
+   submitMsgBtn.disabled=false;
+   submitMsgBtn.classList.remove('btn-disabled');
+  notification.classList.remove('not-visible');
+  notification.classList.add('notification-error');
+  notsentMsg.classList.remove('hidden');
+  setTimeout(() => {
+    notification.classList.add('not-visible');
+    notification.classList.remove('notification-error');    
+    notsentMsg.classList.add('hidden');
+  }, 3000);
+  }
+ 
+
+
+  console.log('msg sent')
+
 });
     }
   };
